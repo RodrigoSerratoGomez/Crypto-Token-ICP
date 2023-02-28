f61ada60f7a08e93b76bcb8e80028ed8823c8e83
RodrigoSerratoGomez <s_o_a_d@hotmail.com>
2023-02-27 23:00:09 -0500

    Commit 01

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { token, canisterId, createActor } from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
+
+function Faucet() {
+  const [isDisable, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
+
+async function handleClick(event) {
+  setDisable(true);
+  const authClient = await AuthClient.create();
+  const identity = await authClient.getIdentity();
+const authenticatedCanister = createActor(canisterId, {
+  agentOptions: {
+    identity,
+  },
+});
+  const result = await authenticatedCanister.payOut();
+  setText(result);
+}
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free Rodrigo's tokens here! Claim 10,000 RRSG coins to your account.</label>
+      <p className="trade-buttons">
+        <button id="btn-payout" onClick={handleClick} disabled={isDisable}>
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
